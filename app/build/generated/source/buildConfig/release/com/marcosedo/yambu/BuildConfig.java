83c104a502e344d3d2c215e3c1ce84a5ae23d094
Marcos Edo <topermaper@gmail.com>
2015-06-09 00:27:05 +0100

    done

---
+++ app/build/generated/source/buildConfig/release/com/marcosedo/yambu/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.marcosedo.yambu;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.marcosedo.yambu";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 6;
+  public static final String VERSION_NAME = "";
+  // Fields from build type: release
+  public static final String DOMAIN = "http://www.topian.comli.com/";
+}
